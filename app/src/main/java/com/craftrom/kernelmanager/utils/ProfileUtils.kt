edcbdc8061dafa5780a3edcb4941220dcf43719d
cocka11 <35626219+cocka11@users.noreply.github.com>
2020-10-28 17:52:46 +0100

    Update ProfileUtils.kt
    For more battery saver

--- app/src/main/java/com/craftrom/kernelmanager/utils/ProfileUtils.kt
+++ app/src/main/java/com/craftrom/kernelmanager/utils/ProfileUtils.kt
@@ -18,16 +18,16 @@ class ProfileUtils {
                     //Little
                     FileUtils.writeFile("/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor", "conservative",  true, true)
                     FileUtils.writeFile("/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq", "614400",  true, true)
-                    FileUtils.writeFile("/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq", "1804800", true, true)
+                    FileUtils.writeFile("/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq", "1363200", true, true)
 
                     //Big
                     FileUtils.writeFile("/sys/devices/system/cpu/cpu4/cpufreq/scaling_governor", "conservative",true, true)
                     FileUtils.writeFile("/sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq", "633600",true, true)
-                    FileUtils.writeFile("/sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq", "1804800",true, true)
+                    FileUtils.writeFile("/sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq", "1401600",true, true)
 
                     //GPU
                     FileUtils.writeFile("/sys/devices/platform/soc/1c00000.qcom,kgsl-3d0/kgsl/kgsl-3d0/min_clock_mhz", "133",true, true)
-                    FileUtils.writeFile("/sys/devices/platform/soc/1c00000.qcom,kgsl-3d0/kgsl/kgsl-3d0/max_clock_mhz", "650",true, true)
+                    FileUtils.writeFile("/sys/devices/platform/soc/1c00000.qcom,kgsl-3d0/kgsl/kgsl-3d0/max_clock_mhz", "510",true, true)
                     FileUtils.writeFile("/sys/module/adreno_idler/parameters/adreno_idler_active", "1",true, true)
                     FileUtils.writeFile("/sys/class/kgsl/kgsl-3d0/devfreq/adrenoboost", "0",true, true)
 
